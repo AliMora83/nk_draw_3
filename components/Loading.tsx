82865849d3ffb94305e607394ee396e8da1b11e6
Daurius Swarts <ali@openmindi.co.za>
2022-10-16 20:14:58 +0200

    Login

---
+++ components/Loading.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+// import PropagateLoader from "react-spinners/PropagateLoader";
+
+function Loading() {
+  return (
+    <div className='bg-gradient-to-tl from-[#080A0B] to-[#1a1a1a] h-screen flex flex-col items-center justify-center'>
+    <div className='items-center mb-10'>
+      <img className='rounded-full h-32 w-32 mb-10 items-center justify-center' 
+      src="favicon.ico" 
+      alt="namka" />
+      <h4 className=''>Loading NAMKA Draw</h4>
+    </div>
+    {/* <PropagateLoader color="#caa969" size={15}/> */}
+  </div>
+  )
+}
+
+export default Loading
--- components/Login.tsx
+++ components/Login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import { useMetamask  } from "@thirdweb-dev/react";
 
